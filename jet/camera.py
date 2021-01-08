0d04539310aefe13b20dfdf3424e2a0d9cb1679b
Tan Yee Jian <tanyeejian@gmail.com>
2021-01-09 02:01:24 +0800

    update

--- jet/camera.py
+++ jet/camera.py
@@ -3,45 +3,136 @@ import numpy as np
 import cv2
 from laser import *
 
+IDLE = -1
+DETECT_BOX = 0
+DETECT_LASER = 1
+
 display_fps = False
+state = IDLE
 
-
-cap = cv2.VideoCapture(1)
+box = None
+cap = cv2.VideoCapture(0)
 time1 = time.time()
 
-while(True):
-    # Get camera frame
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow('frame',gray)
+# phase 1: detecting rectangle
+# phase 2: detecting dot and move mouse
 
+# TODO:
+# 1. (double for loop in paper) calibrate skew
+# 2. keep detecting box to 3s, add a key to recalibrate
+# 3. tie up with pyautogui for mouse input
+
+
+past_boxes = []
+
+
+def detect_box(gray):
     # Get contours of rectangle from camera frame
-    ret, thresh = cv2.threshold(gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    canny = cv2.Canny(gray, 110, 255)
+    contours, hierarchy = cv2.findContours(
+        canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
 
-    # Get rotated bounding box of rectangle
+    # algorithm to decide the box among contours
     contour = max(contours, key=cv2.contourArea)
     if contour is not None:
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        img = cv2.drawContours(gray, [box], -1, (0, 255, 0), 3)
+        print(len(past_boxes))
+        past_boxes.append(box)
 
-    # Display FPS (around 30)
-    if display_fps:
-        time2 = time.time()
-        fps = 1/(time2-time1)
-        time1 = time2
-        fps = str(int(fps))
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, fps, (7,70), font, 3, (0,255,0), 2, cv2.LINE_AA)
+    # Show canny edge
+    cv2.imshow("frame", canny)
 
-    # Show camera
-    cv2.imshow('frame', img)
 
-    # Quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+def get_laser_loc_blob(gray):
+    print("blob")
+    detector = cv2.SimpleBlobDetector_create()
+    keypoints = detector.detect(gray)
+    # Show blob
+    # cv2.imshow("frame", res)
+    im_with_keypoints = cv2.drawKeypoints(
+        gray,
+        keypoints,
+        np.array([]),
+        (0, 0, 255),
+        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+    )
+    cv2.imshow("frame", im_with_keypoints)
+
+
+def get_laser_loc(gray):
+    pt = cv2.minMaxLoc(gray)
+    print(pt)
+
+
+def detect_laser(gray):
+    print("detecting laser")
+    laser_loc = get_laser_loc(gray)
+    # if outside_box(laser_loc):
+    #     return
+    # else:
+    #     move_mouse(laser_loc)
+
+
+def most_frequent(lst):
+    stringy_dict = {"".join(box.tolist()): box for box in past_boxes}
+    return stringy_dict[max(set(stringy_dict.keys()), key=stringy_dict.keys().count)]
+
+
+portXcoord, portYcoord = [], []
+
+
+def calibrate_box(box):
+    lu, ld, ru, rd = box
+    width = max(ru[0], rd[0]) - min(lu[0], ld[0])
+    height = max(ld[1], rd[1]) - min(lu[1], ru[1])  # FIXME: doesn't make sense
+    portXcoord, portYcoord = np.zeroes((width, height)), np.zeroes((width, height))
+    # FIXME
+    # for i in range(width):
+    #     for j in range(height):
+    #         start
+
+
+while True:
+    # Get camera frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # to quit
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+    # to recalibrate
+    if cv2.waitKey(1) & 0xFF == ord("r"):
+        print("chg state")
+        state = DETECT_BOX
+
+    # read 100 frames, find the box.
+    # TODO: retrigger this on a certain keystroke, check
+    # if "r" works
+    if state == DETECT_BOX:
+        if len(past_boxes) > 30:
+            # then can deduce box.
+            # box is the one with most occurrences in past_boxes
+            # flattened = np.array([box.flatten() for box in past_boxes])
+            # print(flattened)
+            # unique, counts = np.unique(flattened)
+            # box = unique[np.argmax(counts)]
+            # print()
+            box = past_boxes[98]
+            past_boxes = []
+            calibrate_box(box)
+            state = DETECT_LASER
+        detect_box(gray)
+    elif state == DETECT_LASER:
+        detect_laser(gray)
+    else:  # state = IDLE
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(
+            gray, "Press r to start", (7, 70), font, 3, (0, 255, 0), 2, cv2.LINE_AA
+        )
+        cv2.imshow("frame", gray)
 
 # When everything done, release the capture
 cap.release()
